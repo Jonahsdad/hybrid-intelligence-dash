9cc57d16fb1e16819ba4efc2614b868c9da015e2
Jonahsdad <97812215+Jonahsdad@users.noreply.github.com>
2025-11-14 15:05:13 -0600

    Update gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,5 +1,8 @@
 import os
-bind = f"0.0.0.0:{os.getenv('PORT','8000')}"
+bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
 workers = int(os.getenv("WEB_CONCURRENCY", "2"))
 worker_class = "uvicorn.workers.UvicornWorker"
-timeout = 60
+timeout = 90
+keepalive = 5
+accesslog = "-"
+errorlog = "-"
